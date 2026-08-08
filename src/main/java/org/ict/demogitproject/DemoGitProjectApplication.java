0a283865f101b111c2b17ef9055d1f3dde79475e
kimhan <kimhanee2021@gmail.com>
2026-08-08 12:11:18 +0900

    최초 스프링 부트 프로젝트 생성

---
+++ src/main/java/org/ict/demogitproject/DemoGitProjectApplication.java
@@ -0,0 +1,13 @@
+package org.ict.demogitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoGitProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/ict/demogitproject/DemoGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ict.demogitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
