dabd208c83d584b942dd00a6bd70f962018c1ee0
kimhan <kimhanee2021@gmail.com>
2026-08-08 12:32:35 +0900

    HelloWorld 출력

--- src/main/java/org/ict/demogitproject/DemoGitProjectApplication.java
+++ src/main/java/org/ict/demogitproject/DemoGitProjectApplication.java
@@ -8,6 +8,7 @@ public class DemoGitProjectApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoGitProjectApplication.class, args);
+    System.out.println("Hello World!");
   }
 
 }
