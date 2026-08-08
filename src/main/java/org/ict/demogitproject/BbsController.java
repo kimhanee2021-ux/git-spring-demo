9f287731e07ecdd09874fdd9b45eda4478734184
kimhan <kimhanee2021@gmail.com>
2026-08-08 14:17:04 +0900

    index controller 주입

--- src/main/java/org/ict/demogitproject/BbsController.java
+++ src/main/java/org/ict/demogitproject/BbsController.java
@@ -1,5 +1,13 @@
 package org.ict.demogitproject;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BbsController {
+
+  private final IndexController indexController;
+  public BbsController(IndexController indexController) {
+    this.indexController = indexController;
+  }
 
 }
