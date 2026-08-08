8e24bfee2587fb669e0b756a6a4325ca65ca312f
kimhan <kimhanee2021@gmail.com>
2026-08-08 12:47:44 +0900

    출력문 추가1

--- src/main/java/org/ict/demogitproject/IndexController.java
+++ src/main/java/org/ict/demogitproject/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
   @GetMapping("/")
   public String index() {
+    System.out.println("Hello World!");
     return "Hello World!";
   }
 }
